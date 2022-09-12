41d07d3db7e4a0eeda75a1851c046cab1a9c7a1b
Christopher <chris.hem10@gmail.com>
2022-09-12 18:13:14 -0500

    Partially finished with Models

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=200)
+    import_href = models.CharField(max_length=200, unique=True, null=True)
+
+    def __str__(self):
+        return self.vin
+
+class SalesPerson(models.Model):
+    name = models.CharField(max_length=200)
+    employee_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone_number = models.PositiveSmallIntegerField
